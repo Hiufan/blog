2f6c286eb7b36bfaa5205e91f9becfd502d7c4ae
mgx <meiguangxin@126.com>
2015-04-25 23:40:38 +0800

    update

--- javascripts/404.js
+++
@@ -1,8 +0,0 @@
-document.title = sitetitle;
-var path = location.pathname;
-if(rss && (path == '/rss' || path == '/rss/' || path == '/feed' || path == '/feed/' || path == '/atom' || path == '/atom/')){
-	location.href = rss;
-}
-else{
-	location.href = '/#!' + path;
-}
